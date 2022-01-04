2648e2deabb12310e0227bf4dc4efb2a4fae45bb
Kaviraj <ksb1910.07@gmail.com>
2022-01-04 21:48:55 +0530

    day 17 complete

---
+++ day 17/KthSmallestNaive.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class KthSmallestNaive
+{
+    public static void main (String[] args) 
+    {
+        int arr[] = new int[]{10,5,30,12};
+        
+        int n = arr.length;int k=2;
+        
+	    System.out.print(kthSmallest(arr,n,k));
+        
+    }
+    
+    static int kthSmallest(int arr[], int n, int k)
+    {   
+        Arrays.sort(arr);
+        return arr[k-1];
+    }
+}
